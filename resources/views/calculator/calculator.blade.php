3acbc5d39baa9faffecd5b327c8df62449af430b
Nabila Veby <billavee@gmail.com>
2024-06-18 22:45:53 +0700

    login page front end

--- resources/views/calculator/calculator.blade.php
+++ resources/views/calculator/calculator.blade.php
@@ -6,22 +6,24 @@
 @section('container')
 
     <div class="">
-        <div class="flex justify-center items-center w-full max-w-md relative">
-            <div class="backdrop-blur-md p-5 rounded-3xl shadow-lg w-full max-w-md">
-                <h2 class="text-2xl font-bold text-center mb-6">Calorie Calculator</h2>
+        <div class="flex items-center justify-center min-h-screen w-100 max-w-md relative">
+            <div class="backdrop-blur-md p-8 rounded-3xl shadow-lg max-w-md text-center">
+                <h2 class="text-2xl font-bold mb-6">Calorie Calculator</h2>
                 <form action="calculate.php" method="POST">
                     {{-- Your Age --}}
-                    <label for="age">Your Age</label>
-                    <input type="number" id="age" name="age" class="w-64 px-5 py-2 border rounded-full" required>
-
-                    <label for="height">Your Height</label>
-                    <input type="number" id="height" name="height" required>
-
-                    <label for="weight">Your Weight</label>
-                    <input type="number" id="weight" name="weight" required>
-
+                    <div class="mb-2">
+                        <input type="number" id="age" name="age" class="w-64 px-5 py-2 box-border h-9 w-32 rounded-full" placeholder="Your Age" required>
+                    </div>
+                    {{-- <label for="height">Your Height</label> --}}
+                    <div class="mb-2">
+                        <input type="number" id="height" name="height" class="w-64 px-5 py-2 box-border h-9 w-32 rounded-full" placeholder="Height" required>
+                    </div>
+                    {{-- <label for="weight">Your Weight</label> --}}
+                    <div class="mb-2">
+                        <input type="number" id="weight" name="weight" class="w-64 px-5 py-2 box-border h-9 w-32 rounded-full" placeholder="Weight" required>
+                    </div>
                     <div class="activity-level">
-                        <label>Your Activity</label>
+                        <label class="flex items-center justify-center">Your Activity</label>
                         <input type="radio" id="low" name="activity" value="low">
                         <label for="low">Low Active</label>
 
@@ -36,7 +38,7 @@
                     </div>
         </div>
 
-                <button type="submit">Calculate</button>
+                <button type="submit" class="w-64 bg-orange text-black font-semibold py-2 mt-3 rounded-full hover:bg-orange-700/75 focus:outline-none focus:ring-2 focus:ring-orange-500">Calculate</button>
             </form>
         </div>
     </div>
