ed99ca26244f8e6541f3f4b7471303348c8b7ffb
ayyrinn <aprilliachandradd@gmail.com>
2024-06-21 18:13:12 +0700

    final sebelum revisi

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
-    <link href="{{ asset('css/apps.css') }}" rel="stylesheet">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-</head>
+@extends('body.main')
 
-<body class="bg-gradient-to-r from-pink-200 to-orange-300/75 flex justify-center items-center">
+@section('title', 'Meal Planner')
 
+@section('container')
         <div class="p-0">
             <x-slot name="header">
                     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -20,13 +12,13 @@
             
                 <div class="py-12">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                            <div class="max-w-xl">
+                        <div class="p-4 sm:p-8 py-10 bg-white shadow sm:rounded-lg flex items-center justify-center  w-full" style="width: 60%; margin: auto; padding: 30px;">
+                            <div class="max-w-xl w-full">
                                 @include('profile.partials.update-profile-information-form')
                             </div>
                         </div>
             
-                        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                        {{-- <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                             <div class="max-w-xl">
                                 @include('profile.partials.update-password-form')
                             </div>
@@ -36,9 +28,8 @@
                             <div class="max-w-xl">
                                 @include('profile.partials.delete-user-form')
                             </div>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
         </div>
-</body>
-</html>
+@endsection
